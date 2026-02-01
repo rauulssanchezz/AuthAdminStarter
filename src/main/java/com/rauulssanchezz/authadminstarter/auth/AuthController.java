7ec9b21136b7e88d39b278d9b6218737c3cb2903
rauulssanchezz <rauulssanchezz@gmail.com>
2026-02-01 12:46:10 +0100

    auth controller

--- src/main/java/com/rauulssanchezz/authadminstarter/auth/AuthController.java
+++ src/main/java/com/rauulssanchezz/authadminstarter/auth/AuthController.java
@@ -8,9 +8,8 @@ import com.rauulssanchezz.authadminstarter.user.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
 
 
 
@@ -27,7 +26,8 @@ public class AuthController {
     }
     
     @GetMapping("/register")
-    public String showRegisterPage() {
+    public String showRegisterPage(Model model) {
+        model.addAttribute("user", new User());
         return "auth/register";
     }
     
